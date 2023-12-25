e9e09c771abad3269196ee5b2c8d5109c3c8b488
Miguel da Mota <miguel@damota.de>
2023-12-25 13:47:34 +0100

    updates

--- src/routes/tracks.rs
+++ src/routes/tracks.rs
@@ -2,7 +2,7 @@ use actix_web::{Responder, Result, get, web, HttpRequest};
 use serde::{Deserialize, Serialize};
 use crate::models::track::Track;
 use crate::utils::project::AppData;
-use crate::utils::tokens::service::{ Services};
+use crate::utils::tokens::service::{Services};
 
 #[derive(Deserialize, Serialize)]
 struct SearchResponse {
@@ -19,8 +19,6 @@ pub async fn get_track(req: HttpRequest, data: web::Data<AppData>) -> Result<imp
     let spotify_service = services.get(&Services::Spotify).ok_or_else(|| {
         actix_web::error::ErrorInternalServerError("Spotify service not available")
     })?;
-
-    println!("{}", spotify_service.get_token());
 
     let track = spotify_service.get_track_by_isrc(isrc).await;
 
